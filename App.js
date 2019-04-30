d5901fae3219db4dd4f7286f96bd6b32299658a9
Sagesse <dihambouroslyn@gmail.com>
2019-04-30 20:21:00 +0000

    DesignLogginScreen

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import LoggedOut from './src/screens/LoggedOut'
+export default class App extends React.Component {
+  render() {
+    return <LoggedOut />
+  }
+}
+
